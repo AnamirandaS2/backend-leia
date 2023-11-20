2c5402b973e00c3ffefbbfb8db6ab0d4dd472a59
Ian Silva <raspincel@gmail.com>
2023-11-19 22:25:18 -0300

    feat: update configs

--- server.ts
+++
@@ -1,9 +0,0 @@
-import app from './src/app'
-import 'dotenv/config'
-
-
-app.listen(
-    Number(process.env.PORT), 
-    process.env.HOST as string, 
-    ()=> {console.log(`[SERVER IS ON] Server running on port ${process.env.PORT}`)}
-    )
