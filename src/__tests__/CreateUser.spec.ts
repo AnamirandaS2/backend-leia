1cbfcd3fad0ef9e45be133e656e15e3073a93941
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2023-12-10 18:51:57 -0300

    add Test:User Created

---
+++ src/__tests__/CreateUser.spec.ts
@@ -0,0 +1,41 @@
+import { request, response } from 'express';
+import registerService from '../services/user/register'
+import checkEmailAvailability from '../middlewares/user/checkEmailAvailability'
+import { resetPasswordController } from '../controllers/user.controller.';
+import { prismaMock } from '../database/singleton';
+import {v4 as uuidv4} from 'uuid';
+import hash from 'bcrypt';
+
+
+describe('Criação de um Usuario', () => {
+
+    const userLogin = {
+            email : "teste@gmail.com", 
+            name : 'TesteMan', 
+            password : '12345678',
+        }
+
+    it("Deve ser Possivel criar um Usuario", async () => { 
+        
+        const user = {
+            id : uuidv4(),
+            name : userLogin.name,
+            email : userLogin.email,
+            password : hash.hashSync(userLogin.password, 12),
+            avatar : null,
+            approved: false,
+            createdAt : new Date(),
+            updatedAt : new Date(),
+        }
+        
+        prismaMock.user.create.mockResolvedValue(user);
+        
+        expect(registerService(userLogin))
+        .resolves
+        .toStrictEqual({
+            ...user, id: undefined, password: undefined, updatedAt: undefined
+        });
+
+    });
+
+});
