aa73ec27add8dc27441721bdf0b2e11edfb14ca3
Alexandre Oliver <alexandreferreiradeoliveira40@gmail.com>
2024-01-12 15:25:10 -0300

    Add Test: insertIdBook Middleware

--- src/__tests__/Middlewares.book.spec.ts
+++ src/__tests__/Middlewares.book.spec.ts
@@ -2,6 +2,7 @@ import { describe, it } from '@jest/globals';
 import { request, response } from 'express';
 
 import checkBook from '../middlewares/book/checkBook';
+import insertIdBook from '../middlewares/book/insertIdBook';
 
 import { book } from './DataUser';
 import { prismaMock } from './config/singleton';
@@ -17,8 +18,23 @@ describe('Testando os Middlewares Book', () => {
     prismaMock.book.findFirst.mockResolvedValueOnce(null);
     await expect(checkBook(request, response, next)).rejects.toThrow('Livro não encontrado');
 
-    prismaMock.book.findFirst.mockResolvedValue(book);
+    prismaMock.book.findFirst.mockResolvedValueOnce(book);
     await expect(checkBook(request, response, next)).resolves.toBe('ok');
 
   });
+
+  it('insertIdBook Middleware: Inserindo Id de um livro corretamente', async () => {
+
+    request.body = { nameBook : 'Livro-Teste' };
+    const next = jest.fn().mockReturnValue('ok');
+
+    prismaMock.book.findFirst.mockResolvedValueOnce(null);
+    await expect(insertIdBook(request, response, next)).rejects.toThrow('Livro não encontrado: Talvez o nome esteja errado');
+
+    const implemention = jest.fn().mockImplementation(({where}) => {return Promise.resolve(book.id);});prismaMock.book.findFirst.mockImplementationOnce(implemention);
+
+    await expect(insertIdBook(request, response, next)).resolves.toBe('ok');
+    
+  });
+
 });
--- src/__tests__/config/singleton.ts
+++ src/__tests__/config/singleton.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+
 import prisma from '../../database/db';
 
 jest.mock('../../database/db', () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
-}))
+}));
 
 beforeEach(() => {
-  mockReset(prismaMock)
-})
+  mockReset(prismaMock);
+});
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
