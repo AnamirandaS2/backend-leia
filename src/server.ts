95b57e9b2206114f8dbd2562c74464b24523c5ed
LucianoASousa <lucianoamorim506@gmail.com>
2025-09-13 13:02:56 -0300

    refactor: update server configuration to listen on all interfaces and simplify logging output

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import app from "./app";
 import "dotenv/config";
 
 const port = Number(process.env.PORT) || 3000;
-const host = (process.env.HOST as string) || "192.168.0.12";
-
-app.listen(port, host, () => {
-  console.log(`[SERVER IS ON] Server running on http://${host}:${port}`);
+        
+app.listen(port, () => {
+  console.log(`[SERVER IS ON] Server running on http://0.0.0.0:${port}`);
 });
